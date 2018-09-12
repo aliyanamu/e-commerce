51a89311a13ce6cdc24303c6c7045e848145b265
aliyanamu <aliyanamu@gmail.com>
2018-09-13 00:31:15 +0700

    fix user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,7 +3,7 @@ let hashPass = require("../helpers/hashPass");
 const User = require("../models/users"),
   ObjectId = require("mongodb").ObjectId;
 
-  console.log(hashPass('apaaja'))
+console.log(hashPass("apaaja"));
 module.exports = {
   list: (req, res) => {
     User.find()
@@ -23,21 +23,23 @@ module.exports = {
     console.log(name, email, password, phone);
     console.log(typeof name, typeof email, typeof password, typeof phone);
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      phone: req.body.phone
-    })
-      .then(user => {
-        res.status(200).json({
-          message: `succesfully added user: ${req.body.name}`
-        });
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: err.message
-        });
+    User.create(
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone
+      },
+      function(err) {
+        if (!err) {
+          res.status(200).json({
+            message: `succesfully added user: ${req.body.name}`
+          });
+        } else {
+          res.status(500).json({
+            message: err.message
+          });
+        }
       });
   },
 
